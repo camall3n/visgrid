31db9d06f2cf5c0b544ed98a66e097445642c83b
Cam Allen <csal@brown.edu>
2021-08-08 19:22:35 -0400

    Add placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+raise NotImplementedError('Setup file has not been configured. It only lists crucial packages.')
+
+packages = [
+    'gmpy',
+    'graphviz',
+    'gym',
+    'imageio',
+    'imageio-ffmpeg',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'Pillow',
+    'scikit-learn',
+    'scipy',
+    'seaborn',
+    'torch',
+    'tqdm',
+    'yapf',
+]
