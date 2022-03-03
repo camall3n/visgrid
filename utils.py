4c02305d181cf76a2787f5846b8177fba3378596
Cam Allen <csal@brown.edu>
2022-03-03 15:16:43 -0500

    Remove unused load_experiment()

--- utils.py
+++ utils.py
@@ -33,33 +33,6 @@ def MI(x, y):
     log_ratio = log_pxy - log_px - log_py
     return np.mean(log_ratio)
 
-def load_experiment(tag, coefs=None):
-    logfiles = sorted(glob.glob(os.path.join('results/logs', tag + '*', 'train-*.txt')))
-    seeds = [f.split('-')[-1].split('.')[0] for f in logfiles]
-    logs = [open(f, 'r').read().splitlines() for f in logfiles]
-
-    def read_log(log, coefs=coefs):
-        results = [json.loads(item) for item in log]
-        fields = results[0].keys()
-        data = dict([(f, np.asarray([item[f] for item in results])) for f in fields])
-        if coefs is None:
-            coefs = {
-                'L_inv': 1.0,
-                'L_fwd': 0.1,
-                'L_cpc': 1.0,
-                'L_fac': 0.1,
-            }
-        if 'L' not in fields:
-            data['L'] = sum([
-                coefs[f] * data[f] if f != 'L_fac' else coefs[f] * (data[f] - 1)
-                for f in coefs.keys()
-            ])
-        return data
-
-    results = [read_log(log) for log in logs]
-    data = dict(zip(seeds, results))
-    return data
-
 def get_good_color(color):
     colorname = color
     colorname = 'gold' if colorname == 'yellow' else colorname
