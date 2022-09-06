b3eb2b666905c278588a394f58adb3e6bf349bb1
Cam Allen <csal@brown.edu>
2022-09-06 17:35:32 -0400

    Force scipy to use env.np_random

--- visgrid/wrappers/rotation.py
+++ visgrid/wrappers/rotation.py
@@ -10,7 +10,7 @@ class RotationWrapper(BaseObservationWrapper):
     def __init__(self, env: gym.Env):
         super().__init__(env)
         n_dims = env.observation_space.shape[0]
-        self.rotation_matrix = special_ortho_group.rvs(n_dims)
+        self.rotation_matrix = special_ortho_group.rvs(n_dims, random_state=self.np_random)
 
     def observation(self, obs):
         return self.rotation_matrix @ obs
