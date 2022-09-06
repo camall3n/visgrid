7040218bd5c04d775f67ad0211417078c9cd41a7
Cam Allen <csal@brown.edu>
2022-09-06 01:11:04 -0400

    Update setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-raise NotImplementedError('Setup file has not been configured. It only lists crucial packages.')
+import setuptools
 
 packages = [
     'gmpy',
@@ -10,6 +10,7 @@ packages = [
     'numpy',
     'pandas',
     'Pillow',
+    'pytest',
     'scikit-learn',
     'scipy',
     'seaborn',
@@ -17,3 +18,27 @@ packages = [
     'tqdm',
     'yapf',
 ]
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="visgrid",
+    author="Cameron Allen",
+    author_email="csal@brown.edu",
+    version="0.0.1",
+    python_requires=">=3.9",
+    packages=setuptools.find_packages(),
+    install_requires=packages,
+    url="https://github.com/camall3n/visgrid",
+    description="RL environments for quickly running image-based grid-world experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Science/Research",
+    ],
+)
