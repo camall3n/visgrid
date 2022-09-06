903211975690231ebc31f512fca3916d40f21439
Cam Allen <csal@brown.edu>
2022-09-06 16:00:26 -0400

    Test helix doesn't modify unselected dims

--- test/test_helix.py
+++ test/test_helix.py
@@ -6,11 +6,11 @@ from visgrid.wrappers.helix import HelixWrapper
 from visgrid.envs.point import PointEnv
 
 def get_curves():
-    ndim = 3
+    ndim = 5
     n_points = 10000
     n_rotations = 4
     action = np.zeros(ndim)
-    action[-1] = 1 / n_points
+    action[2] = 1 / n_points
 
     line_env = PointEnv(ndim)
     helix1_env = HelixWrapper(PointEnv(ndim), rotations_per_unit_z=n_rotations)
@@ -29,7 +29,7 @@ def get_curves():
     )
 
     def get_points(env):
-        env.reset(x=np.array([0.5, 0.0, -0.5]))
+        env.reset(x=np.array([0.5, 0.0, -0.5, 1.0, 1.0]))
         obs = []
         for _ in range(n_points + 1):
             obs.append(env.step(action)[0])
@@ -47,10 +47,10 @@ def visualize_points():
     line_points, helix1_points, helix2_points, helix3_points = get_curves()
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.plot3D(*line_points.T, 'k', label='0')
-    ax.plot3D(*helix1_points.T, 'r', label='1')
-    ax.plot3D(*helix2_points.T, 'b', label='2')
-    ax.plot3D(*helix3_points.T, 'c', label='3')
+    ax.plot3D(*line_points[:, :3].T, 'k', label='0')
+    ax.plot3D(*helix1_points[:, :3].T, 'r', label='1')
+    ax.plot3D(*helix2_points[:, :3].T, 'b', label='2')
+    ax.plot3D(*helix3_points[:, :3].T, 'c', label='3')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
@@ -58,15 +58,40 @@ def visualize_points():
     ax.set_title('Increasing numbers of helix transformations')
     plt.show()
 
-def test_helix_wrapper():
-    line_points, helix1_points, helix2_points, helix3_points = get_curves()
+@pytest.fixture
+def precompute_curves():
+    return get_curves()
+
+@pytest.fixture
+def precompute_line(precompute_curves):
+    return precompute_curves[0]
+
+@pytest.fixture
+def precompute_helix1(precompute_curves):
+    return precompute_curves[1]
+
+@pytest.fixture
+def precompute_helix3(precompute_curves):
+    return precompute_curves[3]
+
+def test_1dim_line(precompute_line):
+    line_points = precompute_line
     line_std = np.std(line_points, axis=0)
-    assert line_std[0] == 0.0 and line_std[1] == 0.0
+    for i in [0, 1, 3, 4]:
+        assert line_std[i] == 0.0
     assert line_std[2] > 0.1
+
+def test_helix1_shape(precompute_helix1):
+    helix1_points = precompute_helix1
     assert np.allclose(helix1_points[0, :2], helix1_points[-1, :2])
     helix_delta = helix1_points[-1, 2] - helix1_points[0, 2]
     assert np.isclose(helix_delta, 1.0)
 
+def test_unmodified_dims(precompute_line, precompute_helix3):
+    line_points = precompute_line
+    helix3_points = precompute_helix3
+    assert np.allclose(line_points[:, 3:], np.ones_like(line_points[:, 3:]))
+    assert np.allclose(helix3_points[:, 3:], line_points[:, 3:])
+
 if __name__ == '__main__':
     visualize_points()
-    test_helix_wrapper()
