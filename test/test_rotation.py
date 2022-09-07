5a44f2f2739065510988e3b81175ccc103619e91
Cam Allen <csal@brown.edu>
2022-09-07 05:43:46 -0400

    Add optional axes arg for RotationWrapper

--- test/test_rotation.py
+++ test/test_rotation.py
@@ -4,7 +4,7 @@ import numpy as np
 from visgrid.wrappers.rotation import RotationWrapper
 from visgrid.envs.point import PointEnv
 
-def get_curves():
+def get_curves(axes=None):
     ndim = 3
     n_points = 100
     n_lines = 100
@@ -14,7 +14,7 @@ def get_curves():
     line_env = PointEnv(ndim)
     rot_envs = []
     for i in range(n_lines):
-        rot_envs.append(RotationWrapper(PointEnv(ndim)))
+        rot_envs.append(RotationWrapper(PointEnv(ndim), axes=axes))
 
     def get_points(env):
         env.reset(x=np.array([0.0, 0.0, -0.5]))
@@ -33,6 +33,19 @@ def get_curves():
 
 def visualize_points():
     line_points, rot_points = get_curves()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot3D(*line_points.T, 'r')
+    for i in range(len(rot_points)):
+        ax.plot3D(*rot_points[i].T, 'k')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('Random rotation transformations')
+    plt.show()
+
+def visualize_points_with_axes():
+    line_points, rot_points = get_curves(axes=(1, 2))
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     ax.plot3D(*line_points.T, 'r')
@@ -64,6 +77,29 @@ def test_rotation_wrapper():
             sim = cosine_similarity(seg1, seg2)
             assert np.isclose(sim, 1.0)
 
+def test_rotation_wrapper_with_axes():
+    line_points, rot_points = get_curves(axes=(1, 2))
+    line_std = np.std(line_points, axis=0)
+    assert line_std[0] == 0.0 and line_std[1] == 0.0
+    assert line_std[2] > 0.1
+
+    def cosine_similarity(a, b):
+        return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+    n_points = len(rot_points)
+    for i in range(n_points):
+        start = rot_points[i][0]
+        end = rot_points[i][1]
+        length = np.linalg.norm(end - start)
+        for point in rot_points[i][1:-1]:
+            seg1 = point - start
+            seg2 = end - start
+            sim = cosine_similarity(seg1, seg2)
+            assert np.isclose(sim, 1.0)
+
+            assert point[0] == 0
+
 if __name__ == '__main__':
     visualize_points()
-    test_rotation_wrapper()
+    visualize_points_with_axes()
+    test_rotation_wrapper_with_axes()
