430b2bdee83b5efe924e786fdf000441a7279008
Cam Allen <csal@brown.edu>
2022-09-14 20:23:17 -0400

    Ensure pytest uses visgrid repo dir if running from above

--- test/conftest.py
+++ test/conftest.py
@@ -10,3 +10,9 @@ if os.getenv('_PYTEST_RAISE', "0") != "0":
     @pytest.hookimpl(tryfirst=True)
     def pytest_internalerror(excinfo):
         raise excinfo.value
+
+# Ensure pytest uses repo dir if running from above (so it uses the correct paths)
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    repo_path = os.path.dirname(request.fspath.dirname)
+    monkeypatch.chdir(repo_path)
