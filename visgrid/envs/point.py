622e192aff559073fa145a3e3baaf381c3a37771
Cam Allen <csal@brown.edu>
2023-01-06 15:57:35 -0500

    Add BoundedPointEnv

--- visgrid/envs/point.py
+++ visgrid/envs/point.py
@@ -17,6 +17,7 @@ class PointEnv(gym.Env):
     def reset(self, x=None, seed: int = None):
         super().reset(seed=seed)
         if x is not None:
+            assert x.shape == self.x.shape
             self.x = np.array(x)
         else:
             self.x = np.zeros(self.ndim)
@@ -25,3 +26,20 @@ class PointEnv(gym.Env):
     def step(self, action):
         self.x += action
         return self.x.copy(), 0, False, False, {}
+
+class BoundedPointEnv(PointEnv):
+    def __init__(self, ndim: int = 5, low=-1.0, high=1.0) -> None:
+        super().__init__(ndim)
+        self.observation_space = gym.spaces.Box(low, high, (self.ndim, ))
+        self.low = low
+        self.high = high
+
+    def step(self, action):
+        _, *rest = super().step(action)
+        if np.any(self.x > self.high):
+            excess = (self.x - self.high)
+            self.x[excess > 0] -= excess[excess > 0]
+        if np.any(self.x < self.low):
+            excess = (self.x - self.low)
+            self.x[excess < 0] -= excess[excess < 0]
+        return self.x.copy(), *rest
